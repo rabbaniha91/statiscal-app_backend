68fc5b67f0431083c9ae90b966f0ab291847e160
rabbaniha <hrabbaniha@gmail.com>
2025-02-08 11:31:58 +0330

    add validate request to route

--- src/modules/user/routes/index.ts
+++ src/modules/user/routes/index.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { login, logout, signin } from "../controllers";
 import { authenticateUser } from "../../../middlewares/authenticate";
 import { loginValidate, signinValidate } from "../../../middlewares/validator";
+import { validateRequest } from "../../../middlewares/validateRequest";
 
 const router = Router();
 
-router.post("/signin", signinValidate(), signin);
-router.post("/login", loginValidate(), login);
+router.post("/signin", signinValidate(), validateRequest, signin);
+router.post("/login", loginValidate(), validateRequest, login);
 
 router.use(authenticateUser);
 router.get("/logout", logout);
